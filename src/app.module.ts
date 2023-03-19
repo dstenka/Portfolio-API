e0c67ff56cc38c88b117cf23ea6d25bbcfc6b1c5
Dominik Stenka <dominik.stenka2309@outlook.com>
2023-03-19 17:50:27 +0100

    implement jwt auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './data/users/users.module';
+
 @Module({
     imports: [
-        // MongooseModule.forRoot('mongodb://localhost:27017/nest-portfolio-api'),
+        ConfigModule.forRoot({ isGlobal: true }),
         AuthModule,
+        UsersModule,
     ],
     controllers: [AppController],
-    providers: [AppService, AuthService],
+    providers: [AppService],
 })
 export class AppModule {}
--- src/auth/strategy/apikey.strategy.ts
+++ src/auth/strategy/apikey.strategy.ts
@@ -1,19 +1,30 @@
 import { HeaderAPIKeyStrategy } from 'passport-headerapikey';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { AuthService } from './../auth.service';
+import { AuthService } from '../auth.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ApiKeyStrategy extends PassportStrategy(
     HeaderAPIKeyStrategy,
     'api-key',
 ) {
-    constructor(private AuthService: AuthService) {
-        super({ header: 'api-key', prefix: '' }, true, async (apiKey, done) => {
-            if (this.AuthService.validateApiKey(apiKey)) {
-                done(null, apiKey);
-            }
-            done(new UnauthorizedException(), null);
-        });
+    constructor(
+        private authService: AuthService,
+        private configService: ConfigService,
+    ) {
+        const headerKeyApiKey =
+            configService.get<string>('HEADER_KEY_API_KEY') || '';
+
+        super(
+            { header: headerKeyApiKey, prefix: '' },
+            true,
+            async (apiKey, done) => {
+                if (this.authService.validateApiKey(apiKey)) {
+                    done(null, true);
+                }
+                done(new UnauthorizedException(), null);
+            },
+        );
     }
 }
